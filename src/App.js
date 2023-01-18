b354f73e91dd2f6dd878ad3156e907eea97af46e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-18 09:06:41 +0700

    update-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {Route, Routes, Link } from "react-router-dom";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className='app'>
+        {/* ======================NAVBAR====================== */}
+        <div className='navbar'>
+
+        </div>
+        {/* ++++++++==============Main===============+++++++++ */}
+        <div className='main'>
+
+        </div>
+        {/* =====================Footer======================= */}
+        <div className='footer'>
+
+        </div>
+      </div>
+    </>
   );
 }
 
