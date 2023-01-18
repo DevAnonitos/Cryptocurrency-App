058ac3abada9f43dffe87c038a49db6e5cc7023f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-18 13:24:58 +0700

    update-navbar@2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ import {
 
 import icon from '../images/cryptocurrency.png';
 
+
 const Navbar = () => {
   return (
     <>
@@ -29,9 +30,28 @@ const Navbar = () => {
               CryptosVeris
             </Link>
           </Typography.Title>
-          <Button className='menu-control-container'>
-
-          </Button>
+          <Menu theme='dark'>
+            <Menu.Item icon={<HomeOutlined />}>
+              <Link to='/'>
+                Home
+              </Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />}>
+              <Link to="/cryptocurrencies">
+                Cryptocurrencies
+              </Link>
+            </Menu.Item> 
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+              <Link to="/exchanges">
+                Exchanges
+              </Link>
+            </Menu.Item> 
+            <Menu.Item icon={<BulbOutlined />}>
+              <Link to="/news">
+                News
+              </Link>
+            </Menu.Item>
+          </Menu>
         </div>
       </div>
     </>
