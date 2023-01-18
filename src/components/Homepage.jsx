4a3882ebb140cec0614aa4883b7886ba8b9a2b47
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-18 21:09:50 +0700

    update-Fix-style

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,14 +14,31 @@ const { Title } = Typography;
 const Homepage = () => {
   return (
     <>
-      <Title level={2} className="heading text-emerald-600">Global Crypto Stats</Title>
+      <Title 
+        level={2} 
+        className="heading text-emerald-600"
+      >
+        Global Crypto Stats
+      </Title>
       <Row gutter={[32, 32]}>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" /></Col>
-        <Col span={12}><Statistic title="Total Exchanges" /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:" /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" /></Col>
-        <Col span={12}><Statistic title="Total Cryptocurrencies"/></Col>
-        <Col span={12}><Statistic title="Total Markets" /></Col>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap:" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies"/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" />
+        </Col>
       </Row>
     </>
   )
